1032557cbcf06d401fe55bd9f7ad1a8d00bba3a7
Shane Weaver <shweaver@microsoft.com>
2020-07-24 12:28:29 -0700

    wip

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,9 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using Windows.UI.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
 
 namespace MockMediaApp
 {
@@ -8,6 +13,38 @@ namespace MockMediaApp
         {
             InitializeComponent();
             DataContext = new MainPageViewModel();
+        }
+
+        private void ItemListView_PointerWheelChanged(object sender, PointerRoutedEventArgs e)
+        {
+            var listView = sender as ListView;
+            var point = e.GetCurrentPoint(listView);
+
+            if (point.Properties.IsHorizontalMouseWheel)
+            {
+                // Required to prevent the CollectionsListView from scrolling.
+                e.Handled = true;
+
+                var scrolledDistance = point.Properties.MouseWheelDelta;
+                ScrollViewer scrollViewer = GetChildOfType<ScrollViewer>(listView);
+                scrollViewer.ChangeView(scrollViewer.HorizontalOffset - scrolledDistance, null, null);
+            }
+        }
+
+        // Helper method:
+        // https://stackoverflow.com/questions/10279092/how-to-get-children-of-a-wpf-container-by-type
+        public static T GetChildOfType<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj == null) return null;
+
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+            {
+                var child = VisualTreeHelper.GetChild(depObj, i);
+
+                var result = (child as T) ?? GetChildOfType<T>(child);
+                if (result != null) return result;
+            }
+            return null;
         }
     }
 }
---
+++ ManipulationInputProcessor.cs
@@ -0,0 +1,94 @@
+﻿using Windows.UI.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Input;
+
+namespace MockMediaApp
+{
+    public class ManipulationInputProcessor
+    {
+        GestureRecognizer recognizer;
+        UIElement element;
+        UIElement reference;
+
+        public ManipulationInputProcessor(GestureRecognizer gestureRecognizer, UIElement target, UIElement referenceFrame)
+        {
+            recognizer = gestureRecognizer;
+            element = target;
+            reference = referenceFrame;
+
+            // The GestureSettings property dictates what manipulation events the
+            // Gesture Recognizer will listen to.  This will set it to a limited
+            // subset of these events.
+            recognizer.GestureSettings = GenerateDefaultSettings();
+            // Set up pointer event handlers. These receive input events that are used by the gesture recognizer.
+            element.PointerPressed += OnPointerPressed;
+            element.PointerMoved += OnPointerMoved;
+            element.PointerReleased += OnPointerReleased;
+            element.PointerCanceled += OnPointerCanceled;
+        }
+
+        // Return the default GestureSettings for this sample
+        GestureSettings GenerateDefaultSettings()
+        {
+            return GestureSettings.ManipulationTranslateX;
+        }
+
+        // Route the pointer pressed event to the gesture recognizer.
+        // The points are in the reference frame of the canvas that contains the rectangle element.
+        void OnPointerPressed(object sender, PointerRoutedEventArgs args)
+        {
+            // Set the pointer capture to the element being interacted with so that only it
+            // will fire pointer-related events
+            element.CapturePointer(args.Pointer);
+            // Feed the current point into the gesture recognizer as a down event
+            recognizer.ProcessDownEvent(args.GetCurrentPoint(reference));
+        }
+
+        // Route the pointer moved event to the gesture recognizer.
+        // The points are in the reference frame of the canvas that contains the rectangle element.
+        void OnPointerMoved(object sender, PointerRoutedEventArgs args)
+        {
+            // Feed the set of points into the gesture recognizer as a move event
+            recognizer.ProcessMoveEvents(args.GetIntermediatePoints(reference));
+        }
+
+        // Route the pointer released event to the gesture recognizer.
+        // The points are in the reference frame of the canvas that contains the rectangle element.
+        void OnPointerReleased(object sender, PointerRoutedEventArgs args)
+        {
+            // Feed the current point into the gesture recognizer as an up event
+            recognizer.ProcessUpEvent(args.GetCurrentPoint(reference));
+            // Release the pointer
+            element.ReleasePointerCapture(args.Pointer);
+        }
+
+        // Route the pointer canceled event to the gesture recognizer.
+        // The points are in the reference frame of the canvas that contains the rectangle element.
+        void OnPointerCanceled(object sender, PointerRoutedEventArgs args)
+        {
+            recognizer.CompleteGesture();
+            element.ReleasePointerCapture(args.Pointer);
+        }
+
+        // Modify the GestureSettings property to enable or disable inertia based on the passed-in value
+        public void UseInertia(bool inertia)
+        {
+            if (!inertia)
+            {
+                recognizer.CompleteGesture();
+                recognizer.GestureSettings ^= GestureSettings.ManipulationTranslateInertia | GestureSettings.ManipulationRotateInertia;
+            }
+            else
+            {
+                recognizer.GestureSettings |= GestureSettings.ManipulationTranslateInertia | GestureSettings.ManipulationRotateInertia;
+            }
+        }
+
+        public void Reset()
+        {
+            element.RenderTransform = null;
+            recognizer.CompleteGesture();
+            recognizer.GestureSettings = GenerateDefaultSettings();
+        }
+    }
+}
